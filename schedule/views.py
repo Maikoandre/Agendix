b36f1c6fc251f55da80afda4de484b86cdcb3aaf
Maiko <ewigemagie@outlook.com>
2025-10-08 17:06:46 -0300

    feat: integrate template

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Session
+import json
 
-# Create your views here.
+def index(request):
+    sessions = Session.objects.all()
+    return render(request, 'dashboard.html', {'sessions': sessions})
